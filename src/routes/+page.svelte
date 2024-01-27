db7c511bf044c553a3aec3563c38fd315b9a480c
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-01-26 21:01:53 -0800

    initial content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,206 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<title>Save Los Altos Tiny Tots and Kinder Prep School Programs</title>
+	<meta name="description" content="Save Los Altos Tiny Tots and Kinder Prep School Programs" />
+
+	<link
+		href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%7CRoboto+Slab:300%7CPT+Serif:700%7CLora"
+		rel="stylesheet"
+		type="text/css"
+	/>
+
+	<meta
+		property="og:site_name"
+		content="Save Los Altos Tiny Tots and Kinder Prep School Programs"
+	/>
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Save Los Altos Tiny Tots and Kinder Prep School Programs" />
+	<meta
+		property="og:description"
+		content="Save Los Altos Tiny Tots and Kinder Prep School Programs"
+	/>
+	<meta property="og:url" content="https://losaltos.school" />
+	<meta
+		property="og:image"
+		content="https://www.soslobby.com/images/social/social_facebook_card.jpg"
+	/>
+</svelte:head>
+
+<main>
+	<section class="cover">
+		<div>
+			<div id="about">
+				<div class="col-sm-12 text-center">
+					<h2>
+						<span class="heading-highlight">PETITION for saving</span>
+						Tiny Tots and Kinder Prep Schools
+					</h2>
+
+					<p>
+						<span class="border-light-dotted">Parents and staff deserve a chance</span> to save these
+						schools.
+					</p>
+
+					<a href="#" class="btn btn-download-petition">Sign Petition</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="letter">
+		<div class="letter-heading">
+			<h1>LETTER</h1>
+
+			<h2>
+				PETITION TO THE CITY MANAGER
+				<br />
+				AND CITY COUNCIL OF LOS ALTOS
+			</h2>
+		</div>
+
+		<div class="letter-content">
+			<p>
+				<span>
+					We were surprised to learn that the City of Los Altos is planning to end the Tiny Tots and
+					Kinder Prep Preschool programs and open up an RFP for corporations to bid on using the
+					preschool facilities.
+				</span>
+			</p>
+
+			<p>
+				<span>
+					The Tiny Tots program has been in existence for 50+ years, with many former students now
+					sending their own children to participate. The program also sets the children up to become
+					engaged citizens through the unique opportunities they have to interact with other
+					community organizations through regular field trips and activities. A quality preschool
+					program with a low student/teacher ratio and an affordable price has countless benefits
+					for not only students, but the community as a whole. High quality early education programs
+					attract residents and prepare students for success in Los Altos schools.
+				</span>
+			</p>
+
+			<p>
+				<span>
+					The program consists of experienced teachers with a passion for early childhood
+					development in an environment that allows the children to learn and thrive. The staff has
+					gone above and beyond to provide a quality experience, even through the trying times of
+					the COVID pandemic. It is disturbing that the city is contemplating a loss of these
+					teaching jobs during a nationwide teacher shortage. The proposed implementation timeline
+					means there is a high risk that no program will be offered next year as it will be
+					extremely challenging to contract with a private company, hire new teachers, and enroll
+					students in time for next school year.
+				</span>
+			</p>
+
+			<p>
+				<span>
+					It is alarming that the city is planning to eliminate this long standing program without
+					providing parents or the community any notice or transparency into the decision making
+					process. Tax dollars should be going to benefit the local community, rather than a private
+					corporation. Outsourcing to a private company will result in a decrease in program
+					quality, an increase in tuition, and the loss of highly experienced and dedicated
+					teachers. We are urging the city to delay their decision and first attempt working with
+					the community to make the Tiny Tots and Kinder Prep programs successful. The parents of
+					the school are highly engaged in the school and community and are asking to have the
+					chance to work together on potential solutions before a decision to shutter our local
+					school is made without consultation of parents, students, teachers, and residents.
+				</span>
+			</p>
+			<p><span>Sincerely Yours,<br />(Signatures are attached)</span></p>
+		</div>
+	</section>
+</main>
+
+<style>
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
+		font-family: 'PT Serif', serif;
+		color: #272727;
+		line-height: 32px;
+		margin: 0 0 16px 0;
+	}
+
+	p {
+		line-height: 32px;
+		margin: 0 0 12px;
+		padding: 0;
+	}
+	h2 + p {
+		margin-top: 12px;
+	}
+
+	section {
+		padding: 6rem 0;
+	}
+
+	.cover {
+		background: black;
+	}
+
+	.cover h2 {
+		text-align: center;
+		font-size: 48px;
+		padding: 20px 48px 0;
+		line-height: 48px;
+		color: #fff;
+	}
+
+	.cover p {
+		color: #fff;
+		display: block;
+		margin-bottom: 32px;
+	}
+
+	.heading-highlight {
+		color: #ff8137;
+		font-family: 'Lora', serif;
+		font-size: 50px;
+		font-style: italic;
+		font-weight: 400;
+		display: block;
+		margin-bottom: 12px;
+	}
+	.border-light-dotted {
+		border-bottom: 1px dotted #ff8137;
+	}
+
+	.letter {
+		background: #f9fafb;
+	}
+
+	.letter-heading {
+		text-align: center;
+	}
+	.letter-heading h1 {
+		color: #ff8137;
+		font-size: 60px;
+		line-height: 36px;
+		position: relative;
+		margin-bottom: 48px;
+	}
+	.letter-heading h2 {
+		font-size: 20px;
+	}
+
+	.letter-content {
+		max-width: 555px;
+		margin: 32px auto 0;
+		font-family: 'Lora', serif;
+		font-style: italic;
+	}
+
+	.letter-content p {
+		font-size: 15px;
+		font-style: italic;
+		font-weight: 400;
+		line-height: 24px;
+	}
+
+	.letter-content p span {
+		border-bottom: 1px solid #e6e6e6;
+	}
+</style>
